6fed123bb3576f7b9643f08a006defe0d76f5a09
germanolsd <germanolsd@gmail.com>
2025-02-08 15:48:42 -0300

    simpler navigation

--- App.tsx
+++ App.tsx
@@ -1,41 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import { Button } from 'react-native';
+import {Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { useLinkTo } from '@react-navigation/native';
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
-import ImageDetailsScreen from './screens/ImageDetailsScreen';
 
 type RootStackParamList = {
-  ImagePicker: undefined;
-  ImageDetails: { imageUri: string };
+  homeScreen: undefined;
   Settings: undefined;
 };
 
-export const ImageContext = React.createContext<{
-  selectedImage: string | null;
-  setSelectedImage: (image: string) => void;
-}>({
-  selectedImage: null,
-  setSelectedImage: () => {},
-});
 const Stack = createNativeStackNavigator<RootStackParamList>();
   
 export default function App() {
   const linkTo = useLinkTo();
-  const [selectedImage, setSelectedImage] = React.useState<string | null>(null);
 
   return (
     <NavigationContainer>
-      <ImageContext.Provider value={{ selectedImage, setSelectedImage }}>
         <Stack.Navigator
           screenOptions={{
             headerShown: true,
@@ -47,32 +29,9 @@ export default function App() {
             ),
           }}
         >
-          <Stack.Screen name="ImagePicker" component={HomeScreen} />
-          <Stack.Screen name="ImageDetails" component={ImageDetailsScreen} />
+          <Stack.Screen name="homeScreen" component={HomeScreen} />
           <Stack.Screen name="Settings" component={SettingsScreen} />
         </Stack.Navigator>
-      </ImageContext.Provider>
-    </NavigationContainer>
-  );
-}
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: true,
-          headerRight: () => (
-            <Button
-              title="Settings"
-              onPress={() => linkTo('Settings') } 
-            />
-          ),
-        }}
-      >
-        <Stack.Screen name="ImagePicker" component={HomeScreen} />
-        <Stack.Screen name="ImageDetails" component={ImageDetailsScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-      </Stack.Navigator>
     </NavigationContainer>
   );
 }
