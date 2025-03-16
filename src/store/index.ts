e4523f43206753f5238209be23cd1ad425124c9b
germanolsd <germanolsd@gmail.com>
2025-03-16 19:07:43 -0300

    feat: enhance ingredient category store with persistence

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,24 @@
 import { create } from "zustand";
+import { UserCategoriesModel } from "../storage/models/userCategories";
+
+const {getAll, swapState} = UserCategoriesModel();
 
 type IngredientCategoryStore = {
   categories: string[];
   setCategories: (categories: string[]) => void;
+  fetch: () => void;
 };
 
 const ingredientsCategoriesStore = create<IngredientCategoryStore>((set) => ({
   categories: [],
-  setCategories: (categories) => {
+  setCategories: async (categories) => {
+    await swapState(categories);
     set({ categories })
   },
+  fetch: async () => {
+    const categories = await getAll();
+    set({ categories });
+  } 
 }));
 
 export {ingredientsCategoriesStore}
